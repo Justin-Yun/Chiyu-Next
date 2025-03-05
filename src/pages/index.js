3373f065b512221860dd90e635098fe423cd60ab
Justin-Yun <36687861+Justin-Yun@users.noreply.github.com>
2025-03-04 18:19:26 -0700

    Edited Index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,10 @@ import Link from 'next/link';
 import Button from '../../components/Button';
 import Navigation from '../../components/Navigation';
 
-
-
 export default function Home() {
-  const handleClick = () => {
-    alert('Button clicked!');
-  };  
-
   const pageContent = (
     <div className="flex flex-col justify-center items-center h-screen">
+      <h1>Add Chiyu Image HERE</h1>
       <h1 className="text-4xl font-semibold mb-8 text-white">Chiyu</h1>
       <Navigation />
     </div>
