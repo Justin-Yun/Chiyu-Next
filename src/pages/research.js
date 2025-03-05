7a3c863cf443b46c7f8591bfee6f6fe4d0e29914
Justin-Yun <36687861+Justin-Yun@users.noreply.github.com>
2025-03-04 22:05:22 -0700

    Fixed title for research.js

--- src/pages/research.js
+++ src/pages/research.js
@@ -7,7 +7,7 @@ import Navigation from '../../components/Navigation';
 export default function Research() {
     const pageContent = (
         <div className="flex flex-col justify-center items-center slide-in-fwd-center">
-            <title>About</title>
+            <title>Research</title>
             <Header />
             <Navigation />
             <h1>Research</h1>
