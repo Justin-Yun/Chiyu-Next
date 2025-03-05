778210d19895790b2297cb9741cc399e23dd7db0
Justin-Yun <36687861+Justin-Yun@users.noreply.github.com>
2025-03-04 20:36:35 -0700

    Added PhotoGallery.js

--- components/PhotoGallery.js
+++ components/PhotoGallery.js
@@ -2,13 +2,14 @@ import Link from 'next/link';
 import Image from 'next/image'
 
 const PhotoGallery = () => {
-    const headerContent = (
+    const photoContent = (
         <div>
             <h3>TEST Photo Gallery</h3>
+            <p>Here&apos;s the gallary</p>
         </div>
     )
 
-    return headerContent;
+    return photoContent;
 };
 
 export default PhotoGallery;
