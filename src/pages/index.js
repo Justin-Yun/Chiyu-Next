61ea68109b361def068562cb8b0e898ed54b7943
Justin-Yun <juyu9703@colorado.edu>
2025-03-22 00:31:50 -0600

    Removed iframe for index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
-import Link from 'next/link';
-import Image from 'next/image';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import Navigation from '../components/Navigation';
-import Particles from '@/components/Particles';
+import Link from "next/link";
+import Image from "next/image";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Navigation from "../components/Navigation";
+import Particles from "@/components/Particles";
 
 export default function Home() {
   const pageContent = (
@@ -11,7 +11,7 @@ export default function Home() {
       {/* Particles Background */}
       <div className="absolute inset-0 z-0">
         <Particles
-          particleColors={['#faedcd', '#faedcd']}
+          particleColors={["#faedcd", "#faedcd"]}
           particleCount={200}
           particleSpread={10}
           speed={0.125}
@@ -27,36 +27,14 @@ export default function Home() {
         <title>CHIYU</title>
         <Header />
         <Navigation />
-        <iframe
-          width="360"
-          height="300"
-          src="https://www.youtube.com/embed/XKd-qdNNko4?si=tN9oWrcq0iItz1Zl"
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          referrerPolicy="strict-origin-when-cross-origin"
-          allowFullScreen
-        ></iframe>
+        <Image
+          src="/icons/Stage_1_Banner-1920x1080.jpg"
+          width={360}
+          height={300}
+        />{" "}
         <Footer />
       </div>
     </div>
   );
-  return pageContent
+  return pageContent;
 }
-
-
-
-//  OLD
-// export default function Home() {
-//   const pageContent = (
-//     // <div className="flex flex-col justify-center items-center h-screen">
-//     <div className="flex flex-col justify-center items-center focus-in">
-//       <title>CHIYU</title>
-//       <Header />
-//       <Navigation />
-//       <iframe width="375" height="300" src="https://www.youtube.com/embed/XKd-qdNNko4?si=tN9oWrcq0iItz1Zl" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-//       <Footer />
-//     </div>
-//   );
-//   return pageContent
-// }
