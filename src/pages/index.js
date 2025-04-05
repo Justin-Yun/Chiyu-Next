6a7d23db15785feba27fbeffebfd684f2b1d836e
Justin-Yun <36687861+Justin-Yun@users.noreply.github.com>
2025-04-04 21:06:44 -0600

    Updated image

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,7 +33,7 @@ export default function Home() {
           // height={300}
           width={400}
           height={350}
-        />
+        />{" "}
         <Footer />
       </div>
     </div>
