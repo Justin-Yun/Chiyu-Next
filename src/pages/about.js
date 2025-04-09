fec21868aec28482bbba62c301ef1e55681d088d
Justin-Yun <juyu9703@colorado.edu>
2025-04-09 14:56:16 -0600

    Updated Research page

--- src/pages/about.js
+++ src/pages/about.js
@@ -34,19 +34,6 @@ const aboutDetails = (
       aesthetics, she is building a strong foundation to adapt to any project
       and bring creative visions to life.
     </p>
-    <p className="text-md text-white-600 dark:text-gray-400">
-      She is currently working on her thesis project, <strong>Chiyu</strong>,
-      which means &quot;healing&quot; in Korean — a video game exploring the emotional
-      journey of a breakup. Through interactive storytelling, the game
-      symbolizes the challenges and growth that come with healing.
-    </p>
-    <p className="text-md text-white-600 dark:text-gray-400">
-      Hannah is researching how design can support emotional processing and
-      incorporating insights from psychology to create a meaningful player
-      experience. Through this project, she is merging creativity with purpose,
-      using interactive design to craft an experience that resonates on a deep
-      emotional level.
-    </p>
   </section>
 );
 
--- src/pages/research.js
+++ src/pages/research.js
@@ -8,10 +8,18 @@ import Particles from "@/components/Particles";
 const researchDetails = (
   <section className="flex flex-col items-center text-center max-w-2xl px-4 mt-6 space-y-6">
     <p className="text-xl md:text-2xl font-medium text-white-800 dark:text-gray-200">
-      Fill in Research Details Here
+      <strong>Chiyu</strong>, which means &quot;healing&quot; in Korean — a
+      video game exploring the emotional journey of a breakup. Through
+      interactive storytelling, the game symbolizes the challenges and growth
+      that come with healing.
     </p>
+    <p className="text-md text-white-600 dark:text-gray-400"></p>
     <p className="text-md text-white-600 dark:text-gray-400">
-      Additional Research Details
+      Hannah is researching how design can support emotional processing and
+      incorporating insights from psychology to create a meaningful player
+      experience. Through this project, she is merging creativity with purpose,
+      using interactive design to craft an experience that resonates on a deep
+      emotional level.
     </p>
   </section>
 );
